41be1f41b4c374b6cef03ff62da78059f520897c
xahmedfathy12@outlook.sa <Ahmed123123>
2019-12-29 15:51:08 +0300

    عرض المواد

--- resources/views/SystemLaws/show.blade.php
+++ resources/views/SystemLaws/show.blade.php
@@ -16,7 +16,7 @@
 @section('content')
 
     <!-- start content-wrapper -->
-    <div class="content-wrapper">
+    <div class="content-wrapper" id="to">
         <div class="main_content">
             <!-- start row -->
             <div class="row align-items-center min-height-row">
@@ -56,8 +56,75 @@
 
                 </div>
             </div>
+            <div class="row align-items-center">
+                <div class="row mt-0">
+                    <div class="col-lg-12 tbl-new-brdr">
+                        <div class="panel panel-default no-brdr">
 
+                            <div class="col-md-3 float-right">
 
+                                <div class="user-block">
+                                    <table class="table table-striped mb-0">
+                                        <thead>
+                                        <tr>
+                                            <th>
+                                                <div class="float-right ml-5"
+                                                     style="padding-top: 12px;">
+                                                    <i class="edit-icon btn-icon-width inline-icon gray-icon"></i>
+                                                    مواد القانون
+                                                </div>
+                                                <form>
+                                                    <div class="input-group search_code">
+                                                        <input type="text" class="form-control"
+                                                               placeholder="رقم المادة">
+                                                    </div>
+                                                </form>
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($law->lawArticles as $article)
+                                            <tr>
+                                                <td>
+                                                    <a class="tab-link" href='#article{{$article->id}}'> <span
+                                                            class="circle"></span>
+                                                        مادة رقم ({{$article->articleno}})
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <div class="col-md-8 float-right">
+                                @foreach($law->lawArticles as $article)
+                                    <div class="article " id="article{{$article->id}}">
+                                        <h1 style="border-bottom: 1px solid #eee;">مادة {{$article->articleno}} </h1>
+                                        <br/>
+                                        <p style="font-size: 21px;text-justify: inter-word;text-align: justify;">
+                                            {{$article->articlebody}}
+                                        </p>
+                                        <a href="{{route('addNewLaw')}}">
+                                            <button class="general_btn btn_1 ml-2">
+                                                <i class="edit-icon btn-icon-width inline-icon green-icon"></i><span>تعديل</span>
+                                            </button>
+                                        </a>
+                                        <a href="{{route('addNewLaw')}}">
+                                            <button class="general_btn btn_1 ml-2">
+                                                <i class="edit-icon btn-icon-width inline-icon green-icon"></i><span>حذف</span>
+                                            </button>
+                                        </a>
+                                        <br/><br/>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="#top">top</a>
         </div>
     </div>
 @endsection
